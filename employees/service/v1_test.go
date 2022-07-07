9ff53f29403035d77dd17d6b3e1c125f6fa906cf
Gaurav Agarwal <algogrit@gmail.com>
2022-07-07 16:49:37 +0530

    Adding a simple makefile to generate code.

---
+++ employees/service/v1_test.go
@@ -0,0 +1,32 @@
+package service_test
+
+import (
+	"testing"
+
+	gomock "github.com/golang/mock/gomock"
+	"github.com/stretchr/testify/assert"
+
+	"algogrit.com/emp-server/employees/repository"
+	"algogrit.com/emp-server/employees/service"
+	"algogrit.com/emp-server/entities"
+)
+
+func TestIndex(t *testing.T) {
+	ctrl := gomock.NewController(t)
+	mockRepo := repository.NewMockEmployeeRepository(ctrl)
+
+	sut := service.NewV1(mockRepo)
+
+	expectedEmps := []entities.Employee{
+		{1, "Gaurav", "LnD", 1001},
+	}
+
+	mockRepo.EXPECT().ListAll().Return(expectedEmps, nil)
+
+	actualEmployees, err := sut.Index()
+
+	assert.Nil(t, err)
+	assert.NotNil(t, actualEmployees)
+
+	assert.Equal(t, expectedEmps, actualEmployees)
+}
