188f7be269ea48bec74cd56c84ec404a21c50cf7
Gaurav Agarwal <algogrit@gmail.com>
2022-07-08 15:12:01 +0530

    Configuring port & db.

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -2,21 +2,46 @@ package main
 
 import (
 	"fmt"
-	"log"
 	"net/http"
 	"os"
 
 	"github.com/gorilla/handlers"
 	"github.com/gorilla/mux"
+	log "github.com/sirupsen/logrus"
+
 	_ "github.com/lib/pq"
+	_ "github.com/mattn/go-sqlite3"
 
 	empHTTP "algogrit.com/emp-server/employees/http"
 	"algogrit.com/emp-server/employees/repository"
 	"algogrit.com/emp-server/employees/service"
 )
 
+func envOrDefault(key, dflt string) string {
+	val, ok := os.LookupEnv(key) // Type: string
+
+	if !ok {
+		log.Info("using default value:", dflt, " for:", key)
+		return dflt
+	}
+
+	return val
+}
+
+var (
+	port     string
+	dbDriver string
+	dbConn   string
+)
+
+func init() {
+	port = envOrDefault("PORT", "8000")
+	dbDriver = envOrDefault("DB_DRIVER", "postgres")
+	dbConn = envOrDefault("DB_CONN", "postgres://localhost:5432/emp-demo?sslmode=disable")
+}
+
 func main() {
-	var empRepo = repository.NewSQL("postgres", "postgres://localhost:5432/emp-demo?sslmode=disable")
+	var empRepo = repository.NewSQL(dbDriver, dbConn)
 	// var empRepo = repository.NewInMem()
 	var empSvcV1 = service.NewV1(empRepo)
 	var empHandler = empHTTP.NewHandler(empSvcV1)
@@ -31,8 +56,8 @@ func main() {
 
 	empHandler.SetupRoutes(r)
 
-	log.Println("Starting server...")
-	err := http.ListenAndServe(":8000", handlers.LoggingHandler(os.Stdout, r))
+	log.Println("Starting server on port:", port, "...")
+	err := http.ListenAndServe(":"+port, handlers.LoggingHandler(os.Stdout, r))
 
 	log.Fatalln(err)
 }
--- go.mod
+++ go.mod
@@ -9,6 +9,8 @@ require (
 	github.com/gorilla/mux v1.8.0
 	github.com/jmoiron/sqlx v1.3.5
 	github.com/lib/pq v1.2.0
+	github.com/mattn/go-sqlite3 v1.14.6
+	github.com/sirupsen/logrus v1.8.1
 	github.com/stretchr/testify v1.8.0
 )
 
@@ -20,7 +22,7 @@ require (
 	github.com/leodido/go-urn v1.2.1 // indirect
 	github.com/pmezard/go-difflib v1.0.0 // indirect
 	golang.org/x/crypto v0.0.0-20220622213112-05595931fe9d // indirect
-	golang.org/x/sys v0.0.0-20220704084225-05e143d24a9e // indirect
+	golang.org/x/sys v0.0.0-20220708085239-5a0f0661e09d // indirect
 	golang.org/x/text v0.3.7 // indirect
 	gopkg.in/yaml.v3 v3.0.1 // indirect
 )
